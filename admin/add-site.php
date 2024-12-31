d3bf4bf01df0008123a0ee1a3688b9fadc731e33
ThatNinjaGuy <vishalgautam.tech@gmail.com>
2024-12-31 12:46:26 +0530

    Refactored add site

--- admin/add-site.php
+++ admin/add-site.php
@@ -1,35 +1,35 @@
 <?php
-    session_start();
-    error_reporting(0);
-    include('../includes/dbconn.php');
-    if(empty($_SESSION['usertype'])) {   
-        header('location:index.php');
-        exit; // Add exit to stop further execution
-    } else {
-    if(isset($_POST['add'])){
+session_start();
+error_reporting(0);
+include('../includes/dbconn.php');
 
-    $name=$_POST['name']; 
-    $city=$_POST['city']; 
-    $sql="INSERT INTO tblsite(name, city) VALUES(:name,:city)";
+if (empty($_SESSION['usertype'])) {
+    header('location:index.php');
+    exit;
+}
+
+if (isset($_POST['add'])) {
+    $name = $_POST['name'];
+    $city = $_POST['city'];
+
+    $sql = "INSERT INTO tblsite(name, city) VALUES(:name, :city)";
     $query = $dbh->prepare($sql);
-    $query->bindParam(':name',$name,PDO::PARAM_STR);
-    $query->bindParam(':city',$city,PDO::PARAM_STR);
+    $query->bindParam(':name', $name, PDO::PARAM_STR);
+    $query->bindParam(':city', $city, PDO::PARAM_STR);
     $query->execute();
     $lastInsertId = $dbh->lastInsertId();
 
-    if($lastInsertId){
+    if ($lastInsertId) {
         header('Location: site.php');
         exit;
     } else {
-        $error="Something went wrong. Please try again";
+        $error = "Something went wrong. Please try again";
     }
-
 }
-
 ?>
 
 <!doctype html>
-<html class="no-js" lang="en">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
@@ -43,19 +43,15 @@
     <link rel="stylesheet" href="../assets/css/metisMenu.css">
     <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="../assets/css/slicknav.min.css">
-    <!-- amchart css -->
     <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <!-- Start datatable css -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.jqueryui.min.css">
-    <!-- others css -->
     <link rel="stylesheet" href="../assets/css/typography.css">
     <link rel="stylesheet" href="../assets/css/default-css.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="stylesheet" href="../assets/css/responsive.css">
-    <!-- modernizr css -->
     <script src="../assets/js/vendor/modernizr-2.8.3.min.js"></script>
     <style>
         .sticky-buttons {
@@ -73,62 +69,49 @@
 </head>
 
 <body>
-    <!-- preloader area start -->
     <div id="preloader">
         <div class="loader"></div>
     </div>
-    <!-- preloader area end -->
-    
+
     <div class="page-container">
-        <!-- sidebar menu area start -->
         <div class="sidebar-menu">
             <div class="sidebar-header">
                 <div class="logo">
-                <a href="dashboard.php">
-    <img src="../assets/images/icon/ar2.jpeg" alt="logo" style="width: 60px; height: auto;">
-</a>
-
+                    <a href="dashboard.php">
+                        <img src="../assets/images/icon/ar2.jpeg" alt="logo" style="width: 60px; height: auto;">
+                    </a>
                 </div>
             </div>
             <div class="main-menu">
                 <div class="menu-inner">
                     <?php
-                        $page='site';
-                        include '../includes/admin-sidebar.php';
+                    $page = 'site';
+                    include '../includes/admin-sidebar.php';
                     ?>
                 </div>
             </div>
         </div>
-        <!-- sidebar menu area end -->
-        <!-- main content area start -->
+
         <div class="main-content">
-            <!-- header area start -->
             <div class="header-area">
                 <div class="row align-items-center">
-                   
                     <div class="col-md-6 col-sm-8 clearfix">
                         <div class="nav-btn pull-left">
                             <span></span>
                             <span></span>
                             <span></span>
                         </div>
-                        
                     </div>
-                    
                     <div class="col-md-6 col-sm-4 clearfix">
                         <ul class="notification-area pull-right">
                             <li id="full-view"><i class="ti-fullscreen"></i></li>
                             <li id="full-view-exit"><i class="ti-zoom-out"></i></li>
-
-                    
-                            <?php include '../includes/admin-notification.php'?>
-
+                            <?php include '../includes/admin-notification.php' ?>
                         </ul>
                     </div>
                 </div>
             </div>
-            <!-- header area end -->
-            <!-- page title area start -->
+
             <div class="page-title-area">
                 <div class="row align-items-center">
                     <div class="col-sm-6">
@@ -137,11 +120,9 @@
                             <ul class="breadcrumbs pull-left">
                                 <li><a href="site.php">Sites</a></li>
                                 <li><span>Add</span></li>
-                                
                             </ul>
                         </div>
                     </div>
-                    
                     <div class="col-sm-6 clearfix">
                         <div class="user-profile pull-right">
                             <img class="avatar user-thumb" src="../assets/images/admin.png" alt="avatar">
@@ -153,15 +134,10 @@
                     </div>
                 </div>
             </div>
-            <!-- page title area end -->
+
             <div class="main-content-inner">
-                
-                
-                <!-- row area start -->
                 <div class="row">
-                    <!-- Dark table start -->
                     <div class="col-12 mt-2">
-                    
                         <div class="card">
                         
 
@@ -185,73 +161,50 @@
                                                 <input class="form-control" name="name" type="text" required id="site-name">
                                             </div>
 
-                                            <div class="form-group col-md-6">
-                                                <label for="site-city" class="col-form-label">City</label>
-                                                <input class="form-control" name="city" type="text" autocomplete="off" required id="site-city">
-                                            </div>
-                                        </div>
-
-                                        <!-- Sticky buttons -->
-                                        <div class="sticky-buttons">
-                                            <button class="btn btn-secondary" type="button" onclick="window.history.back();" style="flex: 1; margin-right: 5px;">Cancel</button>
-                                            <button class="btn btn-primary" name="add" id="add" type="submit" style="flex: 1; margin-left: 5px;">Add</button>
+                                        <div class="form-group col-md-6">
+                                            <label for="site-city" class="col-form-label">City</label>
+                                            <input class="form-control" name="city" type="text" autocomplete="off" required id="site-city">
                                         </div>
                                     </div>
-                                    </form>
-                        </div> 
+
+                                    <!-- Sticky buttons -->
+                                    <div class="sticky-buttons">
+                                        <button class="btn btn-secondary" type="button" onclick="window.history.back();" style="flex: 1; margin-right: 5px;">Cancel</button>
+                                        <button class="btn btn-primary" name="add" id="add" type="submit" style="flex: 1; margin-left: 5px;">Add</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <!-- Dark table end -->
-                    
                 </div>
-                <!-- row area end -->
-                
-                </div>
-                <!-- row area start-->
             </div>
             <?php include '../includes/footer.php' ?>
-        <!-- footer area end-->
         </div>
-        <!-- main content area end -->
-
-        
     </div>
-    <!-- jquery latest version -->
+
     <script src="../assets/js/vendor/jquery-2.2.4.min.js"></script>
-    <!-- bootstrap 4 js -->
     <script src="../assets/js/popper.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
     <script src="../assets/js/owl.carousel.min.js"></script>
     <script src="../assets/js/metisMenu.min.js"></script>
     <script src="../assets/js/jquery.slimscroll.min.js"></script>
     <script src="../assets/js/jquery.slicknav.min.js"></script>
-
-    <!-- start chart js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
     <script src="https://code.highcharts.com/highcharts.js"></script>
-    <!-- start zingchart js -->
     <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
     <script>
     zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
     ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
     </script>
-    <!-- all line chart activation -->
     <script src="assets/js/line-chart.js"></script>
-    <!-- all pie chart -->
     <script src="assets/js/pie-chart.js"></script>
-
-        <!-- Start datatable js -->
         <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
-    
-    <!-- others plugins -->
     <script src="../assets/js/plugins.js"></script>
     <script src="../assets/js/scripts.js"></script>
 </body>
 
 </html>
-
-<?php } ?>
